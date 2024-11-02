c8d0ca4c5a9cb7225be0c495c6d7e8d19ac206ac
ryobui <thienbuid.work@gmail.com>
2024-11-02 12:14:07 +0700

    [fix] fix exception and try catch method sendMail in gmail service

--- src/common/filters/all-exception.filter.ts
+++ src/common/filters/all-exception.filter.ts
@@ -8,7 +8,10 @@ export class AllExceptionsFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
-        const status = exception.getStatus();
+        const status =
+            exception instanceof HttpException
+                ? exception.getStatus()
+                : HttpStatus.INTERNAL_SERVER_ERROR;
 
         let errorResponse: ApiResponse<any> = {
             message:
